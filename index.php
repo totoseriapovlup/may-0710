44578a71b2d7ef8503f7e7788a611c2e794bbdb8
toto <test@test.com>
2022-10-07 16:11:15 +0300

    autoloader

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'working';
+spl_autoload_register(function($className){
+    $classFile = 'app/'.str_replace('\\',DIRECTORY_SEPARATOR, $className).'.php';
+    if(file_exists($classFile)){
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
