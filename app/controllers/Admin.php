f2afa96ac4be58203d5b49dcbabe1421c9418380
toto <test@test.com>
2022-10-10 17:50:35 +0300

    Admin controller

---
+++ app/controllers/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace controllers;
+
+
+use core\AbstractController;
+use core\View;
+use models\ArticleModel;
+
+class Admin extends AbstractController
+{
+    public function __construct()
+    {
+        $this->view = new View('admin');
+    }
+
+    public function index()
+    {
+        //TODO debug of model
+        $model = new ArticleModel();
+        $res = $model->add([
+            'title' => 'test',
+            'text' => 'test text',
+        ]);
+        var_dump($res);
+//        $this->view->render('admin_index');
+    }
+}
